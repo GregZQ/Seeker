5d895c86f5a91f19b3694bcccc7d485dac5f0e83
GregZQ <463803856@qq.com>
2019-03-18 21:55:02 +0800

    auto commit.

--- src/main/java/com/seeker/pipline/Pipline.java
+++ src/main/java/com/seeker/pipline/Pipline.java
@@ -1,9 +1,12 @@
 package com.seeker.pipline;
 
+import us.codecraft.xsoup.XElements;
+
 /**
  * @author GregZQ
  * @create 2019-03-12 21:04
  * @des: 数据管道
  */
-public class Pipline {
+public interface Pipline {
+    void save(XElements value);
 }
